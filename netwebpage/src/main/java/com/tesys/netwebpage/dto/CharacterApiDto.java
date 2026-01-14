584116099d5c502682b25b0df625a82616c4e4d0
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2026-01-14 20:02:01 -0300

    feat: enhance character DTOs with additional fields and create Info class for pagination

--- netwebpage/src/main/java/com/tesys/netwebpage/dto/CharacterApiDto.java
+++ netwebpage/src/main/java/com/tesys/netwebpage/dto/CharacterApiDto.java
@@ -3,16 +3,62 @@ package com.tesys.netwebpage.dto;
 import java.util.List;
 
 public class CharacterApiDto {
-    private Integer id; 
-    
-    private String name; 
 
+    private Integer id;
+    private String name;
     private String status;
-
-    private String species; 
-
-    private String image; 
-
-    // "urls" list
+    private String species;
+    private String image;
     private List<String> episode;
+
+    public CharacterApiDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<String> getEpisode() {
+        return episode;
+    }
+
+    public void setEpisode(List<String> episode) {
+        this.episode = episode;
+    }
 }
--- netwebpage/src/main/java/com/tesys/netwebpage/dto/RickAndMortyResponse.java
+++ netwebpage/src/main/java/com/tesys/netwebpage/dto/RickAndMortyResponse.java
@@ -3,10 +3,26 @@ package com.tesys.netwebpage.dto;
 import java.util.List;
 
 public class RickAndMortyResponse {
+
+    private Info info;
     private List<CharacterApiDto> results;
 
-    // este le pega a /characters de nuestra api
-    public List<CharacterApiDto> getResults(){
+    public RickAndMortyResponse() {
+    }
+
+    public Info getInfo() {
+        return info;
+    }
+
+    public void setInfo(Info info) {
+        this.info = info;
+    }
+
+    public List<CharacterApiDto> getResults() {
         return results;
+    }
+
+    public void setResults(List<CharacterApiDto> results) {
+        this.results = results;
     }
 }
