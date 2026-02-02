767adf6858ecfa40acbb3a1c927a35a39e66b3d2
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2026-02-02 19:33:41 -0300

    refactor: remove unused import for GetMapping in AuthController

--- netwebpage/src/main/java/com/tesys/netwebpage/controller/auth/AuthController.java
+++ netwebpage/src/main/java/com/tesys/netwebpage/controller/auth/AuthController.java
@@ -1,6 +1,5 @@
 package com.tesys.netwebpage.controller.auth;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
